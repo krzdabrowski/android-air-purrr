3375fa227aa85fef0e9b087c9ac61c0443cd5d86
krzdabrowski <krz.dabrowski95@gmail.com>
2019-04-09 20:50:58 +0200

    update TODOs

--- AirPurrr/app/src/main/java/com/example/trubul/airpurrr/view/MainActivity.kt
+++ AirPurrr/app/src/main/java/com/example/trubul/airpurrr/view/MainActivity.kt
@@ -27,15 +27,19 @@ import timber.log.Timber
 // TODO: implement databinding
 // TODO: implement MVVM with LiveData
 // TODO: airly API instead of public
+// TODO: DEPRECATIONS - deal with every single deprecated library to use AndroidX version (or alternative other library -> for ex. ProgressDialog, AuthCallback)
 
-// TODO: (later) DEPRECATIONS - deal with every single deprecated library to use AndroidX version (or alternative other library -> for ex. ProgressDialog, AuthCallback)
 // TODO: (later) UI REWORK - implement TabLayout with current and predicted results/data in fragments
 // TODO: (later) UI REWORK - switch manual control on toolbar, alwaysOn & remove switch with logic
 // TODO: (later) UI REWORK - Preferences instead of menu + settings menu with alwaysOn state (dialogs inflated from new file instead on making everything in Java code (lecture 255 on Udemy))
 // TODO: (later) UI REWORK - selectors for menu items on toolbar
+// TODO: (later) UI REWORK - Navigation Component
+// TODO: (later) UI REWORK - animations
 
-// TODO: (if time) Navigation Component
-// TODO: (if time) animations
+// TODO: (RE-IMPLEMENT) Automatic Mode logic & threshold dialog
+// TODO: (RE-IMPLEMENT) Location
+// TODO: (RE-IMPLEMENT) Fingerprint Reader logic & network/fingerprint checks and permissions
+
 // TODO: (at the end) implement good practices (https://github.com/ribot/android-guidelines/blob/master/project_and_code_guidelines.md)
 // TODO: (at the end) check if all ids are needed and are correct with good practices
 
