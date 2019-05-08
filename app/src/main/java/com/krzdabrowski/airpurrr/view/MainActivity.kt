65acfc84cd6bb754d9c967025d8d76357c7bf718
kdabrowski <kdabrowski@freeportmetrics.com>
2019-05-08 13:41:26 +0200

    add default Navigation transitions/animations

--- app/src/main/java/com/krzdabrowski/airpurrr/view/MainActivity.kt
+++ app/src/main/java/com/krzdabrowski/airpurrr/view/MainActivity.kt
@@ -8,15 +8,13 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.krzdabrowski.airpurrr.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+    private val navController by lazy { findNavController(R.id.main_fragment_host) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = findNavController(R.id.main_fragment_host)
         setupActionBarWithNavController(navController)
-
         addOnBackPressedCallback {
             if (!navController.navigateUp()) moveTaskToBack(true)
             true
