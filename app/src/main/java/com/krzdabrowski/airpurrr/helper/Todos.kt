797d62bd053ae8c070690c1646e85a2ca5778374
kdabrowski <kdabrowski@freeportmetrics.com>
2019-05-20 15:55:44 +0200

    remove TODO

--- app/src/main/java/com/krzdabrowski/airpurrr/helper/Todos.kt
+++ app/src/main/java/com/krzdabrowski/airpurrr/helper/Todos.kt
@@ -1,7 +1,5 @@
 package com.krzdabrowski.airpurrr.helper
 
-// TODO: (RE-IMPLEMENT) Fingerprint Reader logic & network/fingerprint checks and permissions
-
 // TODO: (IDEAS) AAC WorkManager instead of TimerTask()
 // TODO: (IDEAS) check out AAC Paging?
 
