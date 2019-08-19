86b18e36247ae93646b7e00b4904e4b5b83a6157
krzdabrowski <krz.dabrowski95@gmail.com>
2019-08-19 20:07:37 +0200

    move Koin and Timber instantiation to Application

---
+++ app/src/main/java/com/krzdabrowski/airpurrr/common/App.kt
@@ -0,0 +1,21 @@
+package com.krzdabrowski.airpurrr.common
+
+import android.app.Application
+import com.krzdabrowski.airpurrr.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(listOf(networkModule, helperModule, repositoryModule, viewModelModule))
+        }
+    }
+}
--- app/src/main/java/com/krzdabrowski/airpurrr/login/LoginActivity.kt
+++ app/src/main/java/com/krzdabrowski/airpurrr/login/LoginActivity.kt
@@ -6,16 +6,6 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import com.krzdabrowski.airpurrr.R
-import com.krzdabrowski.airpurrr.common.helperModule
-import com.krzdabrowski.airpurrr.common.networkModule
-import com.krzdabrowski.airpurrr.common.repositoryModule
-import com.krzdabrowski.airpurrr.common.viewModelModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import timber.log.Timber
-import timber.log.Timber.DebugTree
 import java.util.concurrent.Executor
 
 class LoginActivity : AppCompatActivity() {
@@ -23,22 +13,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        initLibs()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        stopKoin()
-    }
-
-    private fun initLibs() {
-        Timber.plant(DebugTree())
         FirebaseApp.initializeApp(this)
-        startKoin {
-            androidLogger()
-            androidContext(this@LoginActivity)
-            modules(listOf(networkModule, helperModule, repositoryModule, viewModelModule))
-        }
     }
 
     // code from Retrofit library to get main executor in API <28 (for biometric callback purpose)
